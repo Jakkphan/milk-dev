6e5440ae1a06e84b66cd72900aef8ce79475c503
Jakkphan Boontan <jakkphanbt@gmail.com>
2025-04-04 16:23:49 +0700

    Test update

--- test-github/src/App.jsx
+++ test-github/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Test1 from './components/test1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    
+    <Test1/>
+
+
+    <div>Hello Am am Update</div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ test-github/src/components/test1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Test1 = () => {
+  return (
+    <center>
+        <div
+        style={{
+            width: '200px',
+            height: '200px',
+            backgroundColor: 'blue',
+            padding: '4',
+
+        }}
+    >I am test1test1</div>
+    </center>
+  )
+}
+
+export default Test1
